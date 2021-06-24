966e8adcd3eaaa274b29adfe736ee56a089d4d25
Diana Fitri <diana.fitri@uisi.ac.id>
2021-06-24 19:50:11 +0700

    add kecamatan seeder

--- database/seeders/KecamatanSeeder.php
+++ database/seeders/KecamatanSeeder.php
@@ -17,9 +17,144 @@ class KecamatanSeeder extends Seeder
         Kecamatan::insert([
             [
                 'kec_nama' => 'Balen',
-                'kec_latitude' => '',
-                'kec_longitude' => ''
-            ]
+                'kec_latitude' => '-7.195965',
+                'kec_longitude' => '111.964173'
+            ],
+            [
+                'kec_nama' => 'Baureno',
+                'kec_latitude' => '-7.129435',
+                'kec_longitude' => '112.083407'
+            ],
+            [
+                'kec_nama' => 'Bojonegoro',
+                'kec_latitude' => '-7.154373',
+                'kec_longitude' => '111.886668'
+            ],
+            [
+                'kec_nama' => 'Bubulan',
+                'kec_latitude' => '-7.324584',
+                'kec_longitude' => '111.821087'
+            ],
+            [
+                'kec_nama' => 'Dander',
+                'kec_latitude' => '-7.2531',
+                'kec_longitude' => '111.833011'
+            ],
+            [
+                'kec_nama' => 'Gayam',
+                'kec_latitude' => '-7.192974',
+                'kec_longitude' => '111.719731'
+            ],
+            [
+                'kec_nama' => 'Gondang',
+                'kec_latitude' => '-7.415227',
+                'kec_longitude' => '111.833011'
+            ],
+            [
+                'kec_nama' => 'Kalitidu',
+                'kec_latitude' => '-7.144821',
+                'kec_longitude' => '111.77339'
+            ],
+            [
+                'kec_nama' => 'Kanor',
+                'kec_latitude' => '-7.111643',
+                'kec_longitude' => '112.035714'
+            ],
+            [
+                'kec_nama' => 'Kapas',
+                'kec_latitude' => '-7.199545',
+                'kec_longitude' => '111.89263'
+            ],
+            [
+                'kec_nama' => 'Kasiman',
+                'kec_latitude' => '-7.131624',
+                'kec_longitude' => '111.6303'
+            ],
+            [
+                'kec_nama' => 'Kadewan',
+                'kec_latitude' => '-7.010391',
+                'kec_longitude' => '111.6303'
+            ],
+            [
+                'kec_nama' => 'Kedungadem',
+                'kec_latitude' => '-7.313666',
+                'kec_longitude' => '112.035714'
+            ],
+            [
+                'kec_nama' => 'Kepohbaru',
+                'kec_latitude' => '-7.189974',
+                'kec_longitude' => '112.083407'
+            ],
+            [
+                'kec_nama' => 'Margomulyo',
+                'kec_latitude' => '-7.187941',
+                'kec_longitude' => '111.973115'
+            ],
+            [
+                'kec_nama' => 'Malo',
+                'kec_latitude' => '-7.107912',
+                'kec_longitude' => '111.701845'
+            ],
+            [
+                'kec_nama' => 'Ngambon',
+                'kec_latitude' => '-7.329403',
+                'kec_longitude' => '111.725694'
+            ],
+            [
+                'kec_nama' => 'Ngasem',
+                'kec_latitude' => '-7.248942',
+                'kec_longitude' => '111.764447'
+            ],
+            [
+                'kec_nama' => 'Ngraho',
+                'kec_latitude' => '-7.23637',
+                'kec_longitude' => '111.558754'
+            ],
+            [
+                'kec_nama' => 'Padangan',
+                'kec_latitude' => '-7.193504',
+                'kec_longitude' => '111.606451'
+            ],
+            [
+                'kec_nama' => 'Purwosari',
+                'kec_latitude' => '-7.230467',
+                'kec_longitude' => '111.677997'
+            ],
+            [
+                'kec_nama' => 'Sekar',
+                'kec_latitude' => '-7.410565',
+                'kec_longitude' => '111.725694'
+            ],
+            [
+                'kec_nama' => 'Sugihwaras',
+                'kec_latitude' => '-7.323649',
+                'kec_longitude' => '111.965588'
+            ],
+            [
+                'kec_nama' => 'Sukosewu',
+                'kec_latitude' => '-7.323649',
+                'kec_longitude' => '111.965588'
+            ],
+            [
+                'kec_nama' => 'Sumberejo',
+                'kec_latitude' => '-7.203073',
+                'kec_longitude' => '112.02379'
+            ],
+            [
+                'kec_nama' => 'Tambakrejo',
+                'kec_latitude' => '-7.35759',
+                'kec_longitude' => '112.789697'
+            ],
+            [
+                'kec_nama' => 'Temayang',
+                'kec_latitude' => '-7.332858',
+                'kec_longitude' => '111.907535'
+            ],
+            [
+                'kec_nama' => 'Trucuk',
+                'kec_latitude' => '-7.107997',
+                'kec_longitude' => '111.803201'
+            ],
         ]);
     }
 }
